3d83ac970112bcc63b6fbc8e5040bf30617414b4
Roman Fedchenko <roman.fedchenko@lightningstep.com>
2026-06-01 11:33:03 +0300

    Fix: Add missing steelplast_post_thumbnail() function

--- functions.php
+++ functions.php
@@ -56,3 +56,42 @@ function steelplast_scripts() {
     wp_enqueue_script( 'steelplast-navigation', get_template_directory_uri() . '/js/navigation.js', array(), STEELPLAST_VERSION, true );
 }
 add_action( 'wp_enqueue_scripts', 'steelplast_scripts' );
+
+/**
+ * Displays an optional post thumbnail.
+ *
+ * Wraps the post thumbnail in an anchor element on index views, or a div
+ * element when on single views.
+ */
+function steelplast_post_thumbnail() {
+    if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
+        return;
+    }
+
+    if ( is_singular() ) :
+        ?>
+
+        <div class="post-thumbnail">
+            <?php the_post_thumbnail(); ?>
+        </div><!-- .post-thumbnail -->
+
+    <?php else : ?>
+
+        <a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
+            <?php
+                the_post_thumbnail(
+                    'post-thumbnail',
+                    array(
+                        'alt' => the_title_attribute(
+                            array(
+                                'echo' => false,
+                            )
+                        ),
+                    )
+                );
+            ?>
+        </a>
+
+        <?php
+    endif; // End is_singular().
+}
